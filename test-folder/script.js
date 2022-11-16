9ac25eb3c741e60fca01dd808925ebbc5e3268ae
priyesh <priyesh.ghamandi@questionpro.com>
2022-11-16 11:15:02 +0530

    Updated test case

--- test-folder/script.js
+++ test-folder/script.js
@@ -1,7 +1,7 @@
 const questionpro = require('questionpro')
 
-const qpapi = new questionpro("acd394fc-3f02-4647-b836-18a7b8243937");
+const qpapi = new questionpro("acd394fc-3f02-4647-b836-18a7b82493");
 
-qpapi.getSurveyList(1820007)
+qpapi.getSurveyList(1827)
     .then(data => console.log(data))    
     .catch(e => console.log('ERROR'))
